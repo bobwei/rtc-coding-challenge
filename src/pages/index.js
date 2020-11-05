1215def67bb2799b553df994e44f607bc300b6b2
Bob Wei <bob61016@gmail.com>
2020-11-05 16:12:50 +0800

    Add home page, agora sdk

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import Link from 'next/link';
+import short from 'short-uuid';
 
 const Comp = () => {
-  return <div></div>;
+  const channelId = short.generate();
+  return (
+    <Container>
+      <Row className="section" />
+      <Row>
+        <Col className="text-center">
+          <h1>Welcome to Video Chat</h1>
+        </Col>
+      </Row>
+      <Row className="section" />
+      <Row>
+        <Col className="text-center">
+          <Link href={`/channels/${channelId}`}>
+            <Button>Create Channel</Button>
+          </Link>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Comp;
