43d217f6b212abf57a8962fbedcc5cb524889d4f
Bob Wei <bob61016@gmail.com>
2020-11-05 19:34:01 +0800

    Be able to play one-to-one

--- src/hooks/useRTC/index.js
+++ src/hooks/useRTC/index.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import short from 'short-uuid';
 
-export function useRTC() {
+export function useRTC({ localStreamElementId, remoteStreamElementId }) {
   // prettier-ignore
   const token = '006366e06fdd4ac43869b3c5c181eb8a1c9IADmZIFk3xkt8VQaxNu3zTWiGnrxYFvmVMSIB9KoSf6FY0eO+aIAAAAAEABqf2ZwXhClXwEAAQBdEKVf';
   const appId = '366e06fdd4ac43869b3c5c181eb8a1c9';
@@ -12,9 +12,22 @@ export function useRTC() {
     const p1 = createClient({ appId })
       .then((client) => join({ uid, token, channelId, client }))
       .catch(console.log);
-    Promise.all([p1]);
+    const p2 = createStream({ uid });
+    Promise.all([p1, p2])
+      .then(([client, stream]) => {
+        stream.play(localStreamElementId);
+        client.publish(stream, (err) => console.log(err));
+        client.on('stream-added', (e) => {
+          if (e.stream.getId() !== uid) {
+            client.subscribe(e.stream, (err) => console.log(err));
+          }
+        });
+        client.on('stream-subscribed', (e) => {
+          e.stream.play(remoteStreamElementId);
+        });
+      })
+      .catch(console.log);
   }, []);
-  return {};
 }
 
 function createClient({ appId }) {
@@ -40,6 +53,22 @@ function join({ uid, token, channelId, client }) {
       channelId,
       uid,
       () => resolve(client),
+      (err) => reject(err),
+    );
+  });
+}
+
+function createStream({ uid }) {
+  const AgoraRTC = require('agora-rtc-sdk');
+  return new Promise((resolve, reject) => {
+    const stream = AgoraRTC.createStream({
+      streamID: uid,
+      audio: true,
+      video: true,
+      screen: false,
+    });
+    stream.init(
+      () => resolve(stream),
       (err) => reject(err),
     );
   });
--- src/pages/channels/[id]/index.js
+++ src/pages/channels/[id]/index.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import copy from 'copy-to-clipboard';
 
 import { useRTC } from '../../../hooks/useRTC';
 
 const Comp = () => {
   const router = useRouter();
   const { id: channelId } = router.query;
-  useRTC({ channelId });
+  useRTC({ channelId, localStreamElementId: 'localStream', remoteStreamElementId: 'remoteStream' });
   return (
     <Container>
-      <Row className="section" />
       <Row>
-        <Col>{channelId}</Col>
+        <Col md={{ span: 8, offset: 2 }}>
+          <Row className="section" />
+          <Row className="screens">
+            <Col className="d-flex screen-container">
+              <div id="localStream" className="screen" />
+            </Col>
+            <Col className="d-flex screen-container">
+              <div id="remoteStream" className="screen" />
+            </Col>
+          </Row>
+          <Row className="section" />
+          <Row>
+            <Col>
+              <Button type="primary" block onClick={onInvite}>
+                邀請好友加入
+              </Button>
+            </Col>
+          </Row>
+        </Col>
       </Row>
     </Container>
   );
 };
 
 export default Comp;
+
+function onInvite() {
+  const url = location.href;
+  copy(url);
+  alert('已經複製網址囉，快貼給朋友來邀請他加入吧');
+}
